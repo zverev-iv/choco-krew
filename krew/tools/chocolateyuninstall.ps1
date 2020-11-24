aa623f1e67ecfe0a33b69ff8626198333830b0a1
Илья Зверев <izverev@logrocon.com>
2020-11-24 20:59:11 +0300

    init

---
+++ krew/tools/chocolateyuninstall.ps1
@@ -0,0 +1,6 @@
+﻿$ErrorActionPreference = 'Stop';
+
+#TODO: Uninstall-ChocolateyPath if https://github.com/chocolatey/choco/issues/310 or https://github.com/chocolatey/choco/pull/1019 or https://github.com/chocolatey/choco/pull/166 closed
+
+Remove-Item -Path "$($HOME)\.krew"  -Recurse -Force | Out-Null
+Update-SessionEnvironment
